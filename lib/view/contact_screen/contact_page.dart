11cee44536e390d73f4e298dd5d763e8aa681cd2
thrilokprakashh <thrilokprakashan@gmail.com>
2024-08-30 02:41:21 +0530

    updates

--- lib/view/contact_screen/contact_page.dart
+++ lib/view/contact_screen/contact_page.dart
@@ -8,6 +8,113 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  List contDataList = [
+    {
+      "name": "John Doe",
+      "dp":
+          "https://images.pexels.com/photos/27976680/pexels-photo-27976680/free-photo-of-nigerian-man-in-traditional-yoruba-attire.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Jane Smith",
+      "dp":
+          "https://images.pexels.com/photos/27849560/pexels-photo-27849560/free-photo-of-two-people-standing-in-front-of-a-tree.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Michael Johnson",
+      "dp":
+          "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Emily Davis",
+      "dp":
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "David Wilson",
+      "dp":
+          "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Sophia Martinez",
+      "dp":
+          "https://images.pexels.com/photos/1081677/pexels-photo-1081677.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Daniel Brown",
+      "dp":
+          "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Olivia Taylor",
+      "dp":
+          "https://images.pexels.com/photos/1804512/pexels-photo-1804512.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "James Anderson",
+      "dp":
+          "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Isabella Thomas",
+      "dp":
+          "https://images.pexels.com/photos/27944624/pexels-photo-27944624/free-photo-of-a-woman-in-a-skirt-leaning-against-a-wall.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "William Rodriguez",
+      "dp":
+          "https://images.pexels.com/photos/1231237/pexels-photo-1231237.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Mia Garcia",
+      "dp":
+          "https://images.pexels.com/photos/1144687/pexels-photo-1144687.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Elijah Martinez",
+      "dp":
+          "https://images.pexels.com/photos/374822/pexels-photo-374822.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Ava White",
+      "dp":
+          "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Alexander Lewis",
+      "dp":
+          "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Charlotte Lee",
+      "dp":
+          "https://images.pexels.com/photos/1006110/pexels-photo-1006110.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Benjamin Walker",
+      "dp":
+          "https://images.pexels.com/photos/999267/pexels-photo-999267.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Evelyn Harris",
+      "dp":
+          "https://images.pexels.com/photos/3184298/pexels-photo-3184298.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Lucas Clark",
+      "dp":
+          "https://images.pexels.com/photos/27837778/pexels-photo-27837778/free-photo-of-banca-de-jornal.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Amelia Hall",
+      "dp":
+          "https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    },
+    {
+      "name": "Henry Young",
+      "dp":
+          "https://images.pexels.com/photos/27849553/pexels-photo-27849553/free-photo-of-man-in-shirt-and-sunglasses-lying-down-on-grass.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -164,13 +271,16 @@ class _ContactPageState extends State<ContactPage> {
             ListView.builder(
               physics: NeverScrollableScrollPhysics(),
               shrinkWrap: true,
-              itemCount: 25,
+              itemCount: 21,
               itemBuilder: (context, index) => ListTile(
                 leading: CircleAvatar(
                   radius: 30,
-                  backgroundImage: NetworkImage(""),
+                  backgroundImage: NetworkImage(contDataList[index]["dp"]),
                 ),
-                title: Text("Ava"),
+                title: Text(
+                  contDataList[index]["name"],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
                 subtitle: SizedBox(
                   height: 10,
                 ),
